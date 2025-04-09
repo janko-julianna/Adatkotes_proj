2e70c88dbad923a0d1d1366f4082d01811b921e1
feher-kristof1 <feher.kristof1@students.jedlik.eu>
2025-04-09 13:49:53 +0200

    Állat hozzáadás +

---
+++ menhely/menhely/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menhely
+{
+    public class Animal
+    {
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public bool Adoptable { get; private set; }
+        public int Age { get; private set; }
+        public Animal(string Name, string Type, int Age, string adoptable)
+        {
+            this.Name = Name;
+            this.Type = Type;
+            this.Age = Age;
+            if(adoptable == "Igen")
+            {
+                this.Adoptable = true;
+            }
+            else
+            {
+                this.Adoptable = false;
+            }
+        }
+
+        public Animal()
+        {
+            this.Name = "";
+            this.Type = "";
+            this.Age = 1;
+            this.Adoptable = false;
+        }
+
+    }
+}
